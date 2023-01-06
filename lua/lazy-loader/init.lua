7803281fef5c6df80b9acca29bb3daf6a2d2c006
TheSafdarAwan <thesafdarawan@gmail.com>
2023-01-07 03:05:58 +0500

    todo

--- lua/lazy-loader/init.lua
+++ lua/lazy-loader/init.lua
@@ -69,6 +69,14 @@ function M.load_plugin(plugin)
 	if plugin.on_load and plugin.on_load.reload_buffer then
 		vim.cmd("silent! do BufEnter")
 	end
+
+	if plugin.cmds then
+		for _, cmd in pairs(plugin.cmds) do
+			vim.api.nvim_create_user_command(cmd, function()
+				vim.cmd(cmd)
+			end, {})
+		end
+	end
 end
 
 local default_events = { "BufRead", "BufWinEnter", "BufNewFile" }
@@ -208,6 +216,7 @@ M.load = function(tbl)
 	-- general information about the plugin
 	local plugin = {
 		name = tbl.name,
+		cmds = tbl.cmd or tbl.cmds,
 		before_load = tbl.before_load,
 		on_load = tbl.on_load,
 		_delete_augroup = true,
