155c8b35c892112083292fdb5ec66f164e7717fc
TheSafdarAwan <thesafdarawan@gmail.com>
2023-01-25 14:16:37 +0500

    refactoring

--- lua/lazy-loader/init.lua
+++ lua/lazy-loader/init.lua
@@ -7,11 +7,12 @@ local packer = require("packer")
 local packer_plugins = _G.packer_plugins
 
 local api = vim.api
+local fn = vim.fn
 
 local notify = function(notify)
 	local level = notify.level or vim.log.levels.WARN
 	if not notify then
-		vim.api.nvim_notify("lazy-loader: notify table is not valid", notify.level or level, notify.opts or {})
+		api.nvim_notify("lazy-loader: notify table is not valid", notify.level or level, notify.opts or {})
 		return
 	end
 
@@ -21,14 +22,10 @@ local notify = function(notify)
 	elseif notify.msg and type(notify.msg) == "string" then
 		msg = notify.msg
 	elseif type(notify.msg) == "nil" then
-		vim.api.nvim_notify(
-			"lazy-loader: notify message is not valid",
-			notify.level or level,
-			notify.opts or {}
-		)
+		api.nvim_notify("lazy-loader: notify message is not valid", notify.level or level, notify.opts or {})
 		return
 	end
-	vim.api.nvim_notify(msg, level, notify.opts or {})
+	api.nvim_notify(msg, level, notify.opts or {})
 end
 
 local delete_augroup = function(name)
@@ -38,6 +35,7 @@ end
 ----------------------------------------------------------------------
 --                          Plugin Loader                           --
 ----------------------------------------------------------------------
+local after_plugin_tbl = {}
 function M.load_plugin(plugin)
 	if packer_plugins[plugin.name] and not packer_plugins[plugin.name].enable then
 		-- load the user configuration before loading plugin
@@ -94,13 +92,13 @@ function M.load_plugin(plugin)
 
 	if plugin.cmds then
 		for _, cmd in pairs(plugin.cmds) do
-			vim.api.nvim_create_user_command(cmd, function()
+			api.nvim_create_user_command(cmd, function()
 				vim.cmd(cmd)
 			end, {})
 		end
 	end
 
-	vim.cmd("do User " .. plugin.name .. "has been loaded")
+	vim.cmd("do User " .. plugin.name .. " has been loaded")
 end
 
 local default_events = { "BufRead", "BufWinEnter", "BufNewFile" }
@@ -108,15 +106,15 @@ local default_events = { "BufRead", "BufWinEnter", "BufNewFile" }
 ----------------------------------------------------------------------
 --                         Autocmd Register                         --
 ----------------------------------------------------------------------
-function M.autocmd_register(plugin)
-	local function register_event(tbl)
-		api.nvim_create_autocmd(tbl.events or tbl.event or default_events, {
-			group = api.nvim_create_augroup("lazy_load_" .. tbl.name, { clear = true }),
-			pattern = tbl.pattern,
-			callback = tbl.callback,
-		})
-	end
+local function register_event(tbl)
+	api.nvim_create_autocmd(tbl.events or tbl.event or default_events, {
+		group = api.nvim_create_augroup("lazy_load_" .. tbl.name, { clear = true }),
+		pattern = tbl.pattern,
+		callback = tbl.callback,
+	})
+end
 
+function M.autocmd_register(plugin)
 	local autocmd = plugin.autocmd
 	local events = autocmd.event or autocmd.events
 	-- filetype extension pattern for the autocmd
@@ -188,23 +186,23 @@ local function set_key(key, plugin)
 
 		local extra = ""
 		while true do
-			local c = vim.fn.getchar(0)
+			local c = fn.getchar(0)
 			if c == 0 then
 				break
 			end
-			extra = extra .. vim.fn.nr2char(c)
+			extra = extra .. fn.nr2char(c)
 		end
 
 		local prefix = vim.v.count ~= 0 and vim.v.count or ""
 		prefix = prefix .. "\"" .. vim.v.register
-		if vim.fn.mode("full") == "no" then
+		if fn.mode("full") == "no" then
 			if vim.v.operator == "c" then
 				prefix = "" .. prefix
 			end
 			prefix = prefix .. vim.v.operator
 		end
 
-		vim.fn.feedkeys(prefix, "n")
+		fn.feedkeys(prefix, "n")
 
 		local escaped_keys = api.nvim_replace_termcodes(key.bind .. extra, true, true, true)
 		api.nvim_feedkeys(escaped_keys, "m", true)
@@ -237,13 +235,13 @@ end
 ----------------------------------------------------------------------
 
 function M.after(after_tbl)
-	local packer_path = vim.fn.stdpath("data") .. "/site/pack/packer"
+	local packer_path = fn.stdpath("data") .. "/site/pack/packer"
 	if type(after_tbl.after) ~= "string" then
 		notify({ msg = "after plugin is not a string for: ", level = vim.log.levels.ERROR })
 		return
 	elseif
-		vim.fn.empty(vim.fn.glob(packer_path .. "/start/" .. after_tbl.after)) < 0
-		or vim.fn.empty(vim.fn.glob(packer_path .. "/opt/" .. after_tbl.after)) < 0
+		fn.empty(vim.fn.glob(packer_path .. "/start/" .. after_tbl.after)) < 0
+		or fn.empty(vim.fn.glob(packer_path .. "/opt/" .. after_tbl.after)) < 0
 	then
 		notify(
 			"lazy-loader: "
@@ -255,17 +253,16 @@ function M.after(after_tbl)
 		return
 	end
 
-	local plugin_augroup = vim.api.nvim_create_augroup(
+	local plugin_augroup = api.nvim_create_augroup(
 		"lazy loading " .. after_tbl.name .. " after " .. after_tbl.after,
 		{ clear = true }
 	)
-	vim.api.nvim_create_autocmd("User", {
+	api.nvim_create_autocmd("User", {
 		group = plugin_augroup,
 		pattern = after_tbl.after .. " has been loaded",
 		callback = function()
-			print("hi")
 			M.load_plugin(after_tbl)
-			vim.api.nvim_del_augroup_by_id(plugin_augroup)
+			api.nvim_del_augroup_by_id(plugin_augroup)
 		end,
 	})
 end
@@ -292,6 +289,7 @@ M.load = function(tbl)
 		cmds = tbl.cmd or tbl.cmds,
 		before_load = tbl.before_load,
 		on_load = tbl.on_load,
+		-- TODO: remove this: i know it requires a lot of work but do this anyway
 		_delete_augroup = true,
 	}
 
